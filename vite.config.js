8e3af2801148d848ea9be6ef119f7d572a8b23f8
Wilmer <herrerawilmer.94@gmail.com>
2022-06-27 16:12:17 -0600

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,5 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    jsxFactory: 'h',
-    jsxFragment: 'Fragment'
-  },
-  build: {
-    lib: {
-      entry: './src/main.jsx'
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom']
-    }
-  }
+  plugins: [react()]
 });
